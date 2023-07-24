824c29ed3937ba0d102cd4749f7c60c4017c2d96
Unain <jvdstoel@pm.me>
2023-07-24 21:23:26 +0200

    Added TourListRequest Validation

---
+++ app/Http/Requests/TourListRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TourListRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'priceFrom' => ['numeric'],
+            'priceTo' => ['numeric'],
+            'dateFrom' => ['date'],
+            'dateTo' => ['date'],
+            'sortPrice' => [Rule::in(['asc', 'desc'])],
+        ];
+    }
+}
