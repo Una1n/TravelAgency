2fa6419b73c97de338124f1d0b2cb27c413f1d6d
Unain <jvdstoel@pm.me>
2023-07-24 17:15:30 +0200

    Added HasUuids to models

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tour extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     /** Fillables */
     protected $fillable = [
--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Travel extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     /** Fillable */
     protected $fillable = [
