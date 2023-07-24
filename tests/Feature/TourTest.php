8ab99cc26f98ee9f47c58731042e295158a73b49
Unain <jvdstoel@pm.me>
2023-07-24 22:31:06 +0200

    Added Tour Tests for Filters

--- tests/Feature/TourTest.php
+++ tests/Feature/TourTest.php
@@ -38,3 +38,112 @@ it('can paginate the tours in a specific travel', function () {
     $response->assertJsonPath('meta.last_page', 2);
     $response->assertOk();
 });
+
+it('sorts the tours by starting date in a specific travel', function () {
+    $travel = Travel::factory()->create();
+    $lastTour = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'start_date' => now()->addDays(4),
+        'end_date' => now()->addDays(10),
+    ]);
+    $firstTour = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'start_date' => now(),
+        'end_date' => now()->addDay(),
+    ]);
+
+    $response = getJson(route('tours.index', $travel));
+    $response->assertJsonCount(2, 'data');
+    $response->assertJsonPath('data.0.id', $firstTour->id);
+    $response->assertJsonPath('data.1.id', $lastTour->id);
+    $response->assertOk();
+});
+
+it('sorts the tours by price in a specific travel', function () {
+    $travel = Travel::factory()->create();
+    $cheapTour = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'price_in_cents' => 10000,
+    ]);
+    $expensiveTour = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'price_in_cents' => 20000,
+    ]);
+
+    $response = getJson(route('tours.index', [
+        $travel,
+        'sortPrice' => 'asc',
+    ]));
+    $response->assertJsonCount(2, 'data');
+    $response->assertJsonPath('data.0.id', $cheapTour->id);
+    $response->assertJsonPath('data.1.id', $expensiveTour->id);
+    $response->assertOk();
+});
+
+it('shows the tours by price From/To in a specific travel', function () {
+    $travel = Travel::factory()->create();
+    Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'start_date' => now()->addDays(3),
+        'end_date' => now()->addDays(9),
+        'price_in_cents' => 10000,
+    ]);
+    $cheapTour = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'start_date' => now()->addDays(4),
+        'end_date' => now()->addDays(10),
+        'price_in_cents' => 20000,
+    ]);
+    $expensiveTour = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'start_date' => now()->addDays(5),
+        'end_date' => now()->addDays(11),
+        'price_in_cents' => 40000,
+    ]);
+    Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'start_date' => now()->addDays(6),
+        'end_date' => now()->addDays(12),
+        'price_in_cents' => 50000,
+    ]);
+
+    $response = getJson(route('tours.index', [
+        $travel,
+        'priceFrom' => 200,
+        'priceTo' => 400,
+    ]));
+    $response->assertJsonCount(2, 'data');
+    $response->assertJsonPath('data.0.id', $cheapTour->id);
+    $response->assertJsonPath('data.1.id', $expensiveTour->id);
+    $response->assertOk();
+});
+
+it('shows the tours within start/end date in a specific travel', function () {
+    $travel = Travel::factory()->create();
+    Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'start_date' => now(),
+    ]);
+    $earlyTour = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'start_date' => now()->addDays(4),
+    ]);
+    $lateTour = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'start_date' => now()->addDays(6),
+    ]);
+    Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'start_date' => now()->addDays(7),
+    ]);
+
+    $response = getJson(route('tours.index', [
+        $travel,
+        'dateFrom' => now()->addDays(4)->format('Y-m-d'),
+        'dateTo' => now()->addDays(6)->format('Y-m-d'),
+    ]));
+    $response->assertJsonCount(2, 'data');
+    $response->assertJsonPath('data.0.id', $earlyTour->id);
+    $response->assertJsonPath('data.1.id', $lateTour->id);
+    $response->assertOk();
+});
