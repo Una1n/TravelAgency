bea005fea1e46b64edc209eeb6a097efceed8a88
Unain <jvdstoel@pm.me>
2023-07-24 21:24:10 +0200

    Changed TourController Filter to be cleaner

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -2,36 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TourListRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
-    public function index(Request $request, Travel $travel)
+    public function index(Travel $travel, TourListRequest $request)
     {
-        $tours = $travel->tours();
-
-        if ($request->has('priceFrom')) {
-            $tours = $tours->priceFrom($request->get('priceFrom'));
-        }
-
-        if ($request->has('priceTo')) {
-            $tours = $tours->priceTo($request->get('priceTo'));
-        }
-
-        if ($request->has('dateFrom')) {
-            $tours = $tours->dateFrom(Carbon::create($request->get('dateFrom')));
-        }
-
-        if ($request->has('dateTo')) {
-            $tours = $tours->dateTo(Carbon::create($request->get('dateTo')));
-        }
-
-        if ($request->has('price')) {
-            $tours = $tours->orderBy('price_in_cents', $request->get('price'));
-        }
+        $tours = $travel->tours()
+            ->when($request->query('priceFrom'),
+                fn ($query, string $priceFrom) => $query->priceFrom($priceFrom))
+            ->when($request->query('priceTo'),
+                fn ($query, string $priceTo) => $query->priceTo($priceTo))
+            ->when($request->query('dateFrom'),
+                fn ($query, string $dateFrom) => $query->dateFrom($dateFrom))
+            ->when($request->query('dateTo'),
+                fn ($query, string $dateTo) => $query->dateTo($dateTo))
+            ->when($request->query('sortPrice'),
+                fn ($query, string $order) => $query->orderBy('price_in_cents', $order));
 
         return TourResource::collection(
             $tours->orderBy('start_date')->paginate(10)
