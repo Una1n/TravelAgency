87f4ce50abcca7698f9e08262097ba27c380af50
Unain <jvdstoel@pm.me>
2023-07-24 15:06:15 +0200

    Added User/Role relationships

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
@@ -14,4 +15,9 @@ class Role extends Model
     protected $fillables = [
         'name',
     ];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
