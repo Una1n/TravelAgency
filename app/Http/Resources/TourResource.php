823f7f5591bd9200cf559b76e7f0b68ee156ae35
Unain <jvdstoel@pm.me>
2023-07-24 20:33:08 +0200

    Changed format price in TourResource

--- app/Http/Resources/TourResource.php
+++ app/Http/Resources/TourResource.php
@@ -19,7 +19,7 @@ class TourResource extends JsonResource
             'name' => $this->name,
             'start_date' => $this->start_date,
             'end_date' => $this->end_date,
-            'price' => $this->price,
+            'price' => number_format($this->price, 2),
         ];
     }
 }
