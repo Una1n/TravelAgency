9a21ab59ff64cb4b8c020e3b741164b4a40e15ee
Unain <jvdstoel@pm.me>
2023-07-25 18:57:37 +0200

    Fixed request end_date after start_date

--- app/Http/Requests/StoreTourRequest.php
+++ app/Http/Requests/StoreTourRequest.php
@@ -24,7 +24,7 @@ class StoreTourRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'start_date' => ['required', 'date'],
-            'end_date' => ['required', 'date'],
+            'end_date' => ['required', 'date', 'after:start_date'],
             'price' => ['required', 'regex:/^\d+(\.\d{1,2})?$/'],
         ];
     }
