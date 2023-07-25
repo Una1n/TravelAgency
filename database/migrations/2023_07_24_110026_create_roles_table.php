4c98662996578ebde8804f3a6ac84c02caf78d2b
Unain <jvdstoel@pm.me>
2023-07-25 16:40:05 +0200

    Added RolesSeeder to run on roles migration

--- database/migrations/2023_07_24_110026_create_roles_table.php
+++ database/migrations/2023_07_24_110026_create_roles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Database\Seeders\RoleSeeder;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
+
+        // Add Default Roles on first migration
+        $seeder = new RoleSeeder();
+        $seeder->run();
     }
 
     public function down(): void
