126cd0b478c0426f7e2feaed8880a25399c7b57f
Unain <jvdstoel@pm.me>
2023-07-25 14:45:27 +0200

    Added Authentication Tests

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+use function Pest\Laravel\post;
+
+it('can login as user', function () {
+    $user = User::factory()->create();
+    // Sanctum::actingAs($user, ['*']);
+
+    $response = post(route('login'), [
+        'email' => $user->email,
+        'password' => 'password',
+    ]);
+
+    $response->assertJsonPath('user.id', $user->id);
+    $response->assertJsonPath('authorization.type', 'bearer');
+    $response->assertOk();
+});
+
+it('cant login with invalid credentials', function () {
+    $user = User::factory()->create();
+
+    $response = post(route('login'), [
+        'email' => $user->email,
+        'password' => 'wrong-password',
+    ]);
+
+    $response->assertJsonPath('message', 'Invalid credentials.');
+    $response->assertUnauthorized();
+});
+
+it('can logout as user', function () {
+    $user = User::factory()->create();
+    Sanctum::actingAs($user, ['*']);
+
+    $response = post(route('logout'));
+    $response->assertJsonPath('message', 'Successfully logged out.');
+    $response->assertOk();
+});
